201b7c35488e61f58209b82f5945b39c6c2a86f7
Valeryi Dnkovtsev <valorapaulinio@gmail.com>
2022-11-07 15:33:52 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      int x = 1000; // стоимость билета
+      int y = 20; // колличество рублей за одну милю
+        System.out.println(x / y);
+int w = 50; // колличество миль за билет
+    }
+}

30805a2376c565dbb6c5cabe7361f8abc5715259
Valeryi Dnkovtsev <valorapaulinio@gmail.com>
2022-11-20 13:07:39 +0300

    more commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int price = 1000; // стоимость билета
         int bonusPrice = 20; // колличество рублей за одну милю
-        System.out.println(price / bonusPrice );
-        int bonusAmount = (price / bonusPrice); // сумма бонуса
+
+        int bonusAmount = price / bonusPrice; // сумма бонуса
+        System.out.println(bonusAmount );
 
 
 
